6817321b2a61439e1ffce2c004285195a1227c24
Vivek Bhupatiraju <vivekab@mit.edu>
2024-07-19 20:07:44 +0900

    fully functional hiring query

--- examples/non_interactive_hiring.rs
+++ examples/non_interactive_hiring.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 use phantom_zone::*;
-use rand::{thread_rng, RngCore};
+use rand::{thread_rng, Rng, RngCore};
 
 /**
  * HIRING MP-FHE MATCHING SPEC
@@ -12,7 +12,7 @@ use rand::{thread_rng, RngCore};
 
 const NUM_CRITERIA: usize = 3;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct JobCriteria {
     in_market: bool,                // 0 = not in market, 1 = in market
     position: bool,                 // 0 = hunter, 1 = recruiter
@@ -42,11 +42,11 @@ fn hiring_match(a: JobCriteria, b: JobCriteria) -> bool {
     // if a is recruiter, their criteria is required to be met for a match
     // to be made, vice versa if b is recruiter
     let mut criteria_match = (!a.position | (!a.criteria[0] | b.criteria[0]))
-        | (!b.position | (!b.criteria[0] | a.criteria[0]));
+        & (!b.position | (!b.criteria[0] | a.criteria[0]));
 
     for i in 1..NUM_CRITERIA {
         criteria_match &= (!a.position | (!a.criteria[i] | b.criteria[i]))
-            | (!b.position | (!b.criteria[i] | a.criteria[i]));
+            & (!b.position | (!b.criteria[i] | a.criteria[i]));
     }
 
     both_in_market & compatible_pos & salary_match & criteria_match
@@ -60,11 +60,11 @@ fn hiring_match_fhe(a: FheJobCriteria, b: FheJobCriteria) -> FheBool {
     let salary_match: &FheBool = &((&a.salary.gt(&b.salary)) ^ &b.position);
 
     let mut criteria_match: FheBool = &(&!&a.position | &(&!&a.criteria[0] | &b.criteria[0]))
-        | &(&!&b.position | &(&!&b.criteria[0] | &a.criteria[0]));
+        & &(&!&b.position | &(&!&b.criteria[0] | &a.criteria[0]));
 
     for i in 1..NUM_CRITERIA {
         criteria_match &= &(&!&a.position | &(&!&a.criteria[i] | &b.criteria[i]))
-            | &(&!&b.position | &(&!&b.criteria[i] | &a.criteria[i]));
+            & &(&!&b.position | &(&!&b.criteria[i] | &a.criteria[i]));
     }
 
     &(&(both_in_market & compatible_pos) & salary_match) & &criteria_match
@@ -139,7 +139,7 @@ fn server_extract_job_criteria(id: usize, data: ClientEncryptedData) -> FheJobCr
     let salary = data
         .salary_enc
         .unseed::<Vec<Vec<u64>>>()
-        .key_switch(0)
+        .key_switch(id)
         .extract_at(0);
 
     FheJobCriteria {
@@ -148,6 +148,18 @@ fn server_extract_job_criteria(id: usize, data: ClientEncryptedData) -> FheJobCr
         salary,
         criteria,
     }
+}
+
+/**
+ * FHE DECRYPTION CODE
+ */
+
+fn client_generate_share(ck: ClientKeys, result: FheBool) -> u64 {
+    ck.client_key.gen_decryption_share(&result)
+}
+
+fn client_full_decrypt(ck: ClientKeys, result: FheBool, shares: [u64; 2]) -> bool {
+    ck.client_key.aggregate_decryption_shares(&result, &shares)
 }
 
 fn main() {
@@ -186,21 +198,23 @@ fn main() {
     /*
      * Phase 2: FUNCTION COMPUTATION
      */
+
     println!("\nFunction computation");
 
     // Client encryption
     now = std::time::Instant::now();
+
     let jc_0 = JobCriteria {
         in_market: true,
-        position: false,
-        salary: 100,
-        criteria: [true; NUM_CRITERIA],
+        position: false, // looking for job
+        salary: 100,     // asking for at least 1mil
+        criteria: [true, true, true],
     };
     let jc_1 = JobCriteria {
         in_market: true,
-        position: true,
-        salary: 150,
-        criteria: [true; NUM_CRITERIA],
+        position: true, // recruiter
+        salary: 150,    // can pay up to 1.5mil
+        criteria: [true, false, true],
     };
     let data_0 = client_encrypt_job_criteria(jc_0.clone(), ck_0.clone());
     let data_1 = client_encrypt_job_criteria(jc_1.clone(), ck_1.clone());
@@ -220,15 +234,15 @@ fn main() {
 
     // Server evaluating function
     now = std::time::Instant::now();
-    let match_res = hiring_match(jc_0, jc_1);
+    let match_res = hiring_match(jc_0.clone(), jc_1.clone());
     let match_res_fhe = hiring_match_fhe(jc_fhe_0, jc_fhe_1);
     println!("(3) f1 evaluated, {:?}ms", now.elapsed().as_millis());
 
     // Clients produce decryption share
     now = std::time::Instant::now();
     let decryption_shares = [
-        ck_0.client_key.gen_decryption_share(&match_res_fhe),
-        ck_1.client_key.gen_decryption_share(&match_res_fhe),
+        client_generate_share(ck_0.clone(), match_res_fhe.clone()),
+        client_generate_share(ck_1, match_res_fhe.clone()),
     ];
     println!(
         "(4) Decryption shares generated, {:?}ms",
@@ -237,9 +251,7 @@ fn main() {
 
     // Clients aggregate shares to decrypt
     now = std::time::Instant::now();
-    let out_f1 = ck_0
-        .client_key
-        .aggregate_decryption_shares(&match_res_fhe, &decryption_shares);
+    let out_f1 = client_full_decrypt(ck_0, match_res_fhe, decryption_shares);
     println!(
         "(5) Decryption shares aggregated, data decrypted by client, {:?}ms",
         now.elapsed().as_millis()
@@ -248,4 +260,81 @@ fn main() {
     println!("\nResult comparison");
     println!("Plaintext result: {}", match_res);
     println!("FHE result: {}", out_f1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // cargo test --release --package phantom-zone --example non_interactive_hiring -- --nocapture
+    #[test]
+    fn test_hiring_match() {
+        set_parameter_set(ParameterSelector::NonInteractiveLTE2Party);
+
+        println!("Noninteractive MP-FHE Key Setup");
+
+        // set application's common reference seed
+        let mut seed = [0u8; 32];
+        thread_rng().fill_bytes(&mut seed);
+        set_common_reference_seed(seed);
+
+        // Client setup
+        let ck_0 = client_setup(0, 2);
+        let ck_1 = client_setup(1, 2);
+
+        // Server setup
+        server_setup(vec![
+            ck_0.clone().server_key_share,
+            ck_1.clone().server_key_share,
+        ]);
+
+        // try on 25 random cases to ensure same output as plaintext
+        for i in 1..25 {
+            println!("Running test {}", i);
+
+            // Client encryption
+            let jc_0 = JobCriteria {
+                in_market: thread_rng().gen_bool(0.95),
+                position: thread_rng().gen_bool(0.2),
+                salary: thread_rng().gen_range(0..120),
+                criteria: [
+                    thread_rng().gen_bool(0.8),
+                    thread_rng().gen_bool(0.8),
+                    thread_rng().gen_bool(0.8),
+                ],
+            };
+            let jc_1 = JobCriteria {
+                in_market: thread_rng().gen_bool(0.95),
+                position: thread_rng().gen_bool(0.8),
+                salary: thread_rng().gen_range(80..200),
+                criteria: [
+                    thread_rng().gen_bool(0.8),
+                    thread_rng().gen_bool(0.2),
+                    thread_rng().gen_bool(0.8),
+                ],
+            };
+            let data_0 = client_encrypt_job_criteria(jc_0.clone(), ck_0.clone());
+            let data_1 = client_encrypt_job_criteria(jc_1.clone(), ck_1.clone());
+
+            // Server extracting data from ciphertext
+            let jc_fhe_0 = server_extract_job_criteria(0, data_0);
+            let jc_fhe_1 = server_extract_job_criteria(1, data_1);
+
+            // Server evaluating function
+            let match_res = hiring_match(jc_0.clone(), jc_1.clone());
+            let match_res_fhe = hiring_match_fhe(jc_fhe_0, jc_fhe_1);
+
+            // Clients produce decryption share
+            let decryption_shares = [
+                client_generate_share(ck_0.clone(), match_res_fhe.clone()),
+                client_generate_share(ck_1.clone(), match_res_fhe.clone()),
+            ];
+
+            // Clients aggregate shares to decrypt
+            let out_f1 = client_full_decrypt(ck_0.clone(), match_res_fhe, decryption_shares);
+
+            assert_eq!(match_res, out_f1);
+            println!("{} {:#?} {:#?}", match_res, jc_0, jc_1);
+        }
+    }
 }
